0d20e8d45313d01709b9cc570bea2b86b373a659
Ashwin Ramesh <ashwinr64@gmail.com>
2016-06-09 22:50:40 +0530

    athene: Initial dump from MPJ24.139-23.1

--- cm.mk
+++ cm.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/motorola/lux/full_lux.mk)
+$(call inherit-product, device/motorola/athene/full_lux.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -22,11 +22,11 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := lux
-PRODUCT_NAME := cm_lux
+PRODUCT_DEVICE := athene
+PRODUCT_NAME := cm_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := lux
+PRODUCT_RELEASE_NAME := athene
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Moto X Play"
+    PRODUCT_NAME="Moto G4"
