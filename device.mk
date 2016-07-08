8966059cb40fd8dbbfb200695cd861f9fb11b1de
HostZero <haneesh.da.rocker@gmail.com>
2016-07-08 14:15:26 +0530

    shamrock: keylayout added

--- device.mk
+++ device.mk
@@ -213,6 +213,13 @@ PRODUCT_PACKAGES += \
     tcpdump \
     wcnss_service
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-google
