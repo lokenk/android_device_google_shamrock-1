68b77e875f910212136b3de5b608fc5c54ed068e
HostZero <haneesh.da.rocker@gmail.com>
2016-07-08 15:01:16 +0530

    shamrock: target shamrock device

--- Android.mk
+++ Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter athene,$(TARGET_DEVICE)),)
+ifneq ($(filter shamrock,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
