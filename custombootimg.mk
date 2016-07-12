10499d4599f1af3256ce6701562e6bafe3fd74c6
HostZero <haneesh.da.rocker@gmail.com>
2016-07-12 21:14:17 +0530

    shamrock: quick swap of boot.mk

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,26 +1,11 @@
 LOCAL_PATH := $(call my-dir)
-device_path := device/google/shamrock
-uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
-$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
-zcat $< > $@
-DEVICE_ROOTDIR := device/google/shamrock/rootdir/etc
+MKELF := device/google/shamrock/tools/mkelf.py
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(device_path)/kernel $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/keycheck $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
-$(call pretty,"Boot image: $@")
-$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
-$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
-$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
-$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
-$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/sbin/
-$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/combinedroot/sbin/busybox_init
-$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
-$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
-$(hide) $(MKBOOTIMG) --kernel $(device_path)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_BOOTIMAGE_TARGET)
+$(INSTALLED_BOOTIMAGE_TARGET): $(DEVICE_KERNEL)/kernel $(INSTALLED_RAMDISK_TARGET) $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
+	$(call pretty,"Boot image: $@")
+	$(hide) python $(MKELF) -o $@ $(DEVICE_KERNEL)/kernel $(PRODUCT_OUT)/ramdisk.img
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-$(recovery_ramdisk) \
-$(recovery_kernel)
-$(call build-recoveryimage-target, $@)
-@echo ----- Making recovery image ------
-$(hide) $(MKBOOTIMG) --kernel $(device_path)/kernel --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_RECOVERYIMAGE_TARGET)
-@echo ----- Made recovery image -------- $@
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
+	$(call pretty,"Recovery image: $@")
+	$(hide) python $(MKELF) -o $@ $(DEVICE_KERNEL)/kernel $(PRODUCT_OUT)/ramdisk-recovery.img,ramdisk
+#	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
